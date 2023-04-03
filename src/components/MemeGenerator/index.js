6e4f2845c9a25d628fbf186d6a07c99b65bff808
Shashank Reddy <shashank.godala@gmail.com>
2023-04-02 21:42:01 -0700

    second commit

--- src/components/MemeGenerator/index.js
+++ src/components/MemeGenerator/index.js
@@ -1,3 +1,18 @@
+import {Component} from 'react'
+import {
+  PageContainer,
+  FormContainer,
+  UrlLabel,
+  UrlInput,
+  SelectFont,
+  CustomOption,
+  CustomButton,
+  ImageContainer,
+  TopText,
+  BottomText,
+  Heading,
+} from './styledComponents'
+
 const fontSizesOptionsList = [
   {
     optionId: '8',
@@ -29,3 +44,77 @@ const fontSizesOptionsList = [
   },
 ]
 // Write your code here
+class MemeGenerator extends Component {
+  state = {
+    url: '',
+    topText: '',
+    bottomText: '',
+    fontSize: fontSizesOptionsList[0].displayText,
+  }
+
+  submitForm = event => {
+    event.preventDefault()
+    const {url, topText, bottomText, fontSize} = this.state
+
+    this.setState({
+      url,
+      topText,
+      bottomText,
+      fontSize,
+    })
+  }
+
+  render() {
+    const {url, topText, bottomText, fontSize} = this.state
+    return (
+      <PageContainer>
+        <FormContainer onSubmit={this.submitForm}>
+          <Heading>Meme Generator</Heading>
+          <UrlLabel htmlFor="url">Image Url</UrlLabel>
+          <UrlInput
+            placeholder="Enter the Image Url"
+            id="url"
+            value={url}
+            onChange={e => this.setState({url: e.target.value})}
+          />
+
+          <UrlLabel htmlFor="url">Top Text</UrlLabel>
+          <UrlInput
+            placeholder="Enter the Top Text"
+            id="url"
+            value={topText}
+            onChange={e => this.setState({topText: e.target.value})}
+          />
+
+          <UrlLabel htmlFor="url">Bottom Text</UrlLabel>
+          <UrlInput
+            placeholder="Enter the Bottom Text"
+            id="url"
+            value={bottomText}
+            onChange={e => this.setState({bottomText: e.target.value})}
+          />
+
+          <UrlLabel htmlFor="url">Font Size</UrlLabel>
+
+          <SelectFont
+            onChange={e => this.setState({fontSize: e.target.value})}
+            value={fontSize}
+          >
+            {fontSizesOptionsList.map(item => (
+              <CustomOption key={item.optionId} value={item.displayText}>
+                {item.displayText}
+              </CustomOption>
+            ))}
+          </SelectFont>
+          <CustomButton type="submit">Generate</CustomButton>
+        </FormContainer>
+        <ImageContainer bg={url}>
+          <TopText fontSize={fontSize}>{topText}</TopText>
+          <BottomText fontSize={fontSize}>{bottomText}</BottomText>
+        </ImageContainer>
+      </PageContainer>
+    )
+  }
+}
+
+export default MemeGenerator
--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -1,1 +1,43 @@
 // Style your components here
+import styled from 'styled-components'
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 30px;
+`
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-right: 100px;
+`
+
+export const Heading = styled.h1``
+export const UrlLabel = styled.label``
+export const UrlInput = styled.input``
+export const SelectFont = styled.select``
+export const CustomOption = styled.option``
+export const CustomButton = styled.button`
+  width: 200px;
+  margin-top: 10px;
+`
+export const ImageContainer = styled.div`
+  background-image: url(${props => props.bg});
+  background-size: cover;
+  height: 60vh;
+  min-width: 60vw;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+export const ImageCard = styled.img``
+export const TopText = styled.h1`
+  font-size: ${props => props.fontSize}px;
+  color: white;
+`
+export const BottomText = styled.h1`
+  font-size: ${props => props.fontSize}px;
+  color: white;
+`
